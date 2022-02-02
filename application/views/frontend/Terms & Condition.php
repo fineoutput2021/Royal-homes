e75b577b42c4ec32d12c8fa116fd31ea4b935a42
fineoutput2021 <office.fineoutput@gmail.com>
2022-02-02 16:48:26 +0530

    create pages as following: terms&condition,faq,privacypolicy,aboutus,shipping and dilevery

---
+++ application/views/frontend/Terms & Condition.php
@@ -0,0 +1,30 @@
+<div class="container">
+       <h2>Terms &amp; Conditions</h2>
+       <p>
+           By visiting this website, you unconditionally agree with our terms and conditions. We have the right to reassess or review these terms and conditions at any time at our sole discretion. Each purchase made through the services of this website shall be subject to these Online Shopping Terms and Conditions. This site is possessed and operated by Basant. The domain name&nbsp;<a href="https://www.Royalhomes.in">www.royalhomes.in</a>&nbsp;i.e. the website is registered in the name of Basant, with its registered office at &nbsp;175/A, Circuit House Rd, Ajit Colony, Jodhpur, Rajasthan 342006.
+       </p>
+       <ul>
+
+
+       <li>We are making every possible effort to produce our products as perfectly as possible. What you will observe on the website is decided by your computer screens like colors, shades, textures, sizes, and appearances, etc. So a customer can demand photographs of products other than the ones displayed online, through email at <a href="mailto:care@Royalhomes.in">care@Royalhomes.</a></li>
+
+       <li>The website is specifically designed and protected by copyright. Imitation in any form is strictly forbidden.</li>
+
+       <li>Royalhomes holds all social media posted by the company on Google Plus, Facebook, Twitter, YouTube, Pinterest, and Instagram under its copyright act.</li>
+
+       <li>
+           Action must be taken as per the jurisdiction of the Supreme Court of India for any kind of plagiarism.</li>
+
+       <li>Our website might have links to third party sites that we are not responsible for. We hold no responsibility and accountability for the material posted on these outside websites.</li>
+
+       <li>You shall not upload, show, host, alter, circulate, transfer, inform, list or share unrightfully any piece of information that: (a) is a property of someone other than you; (b) is unacceptably damaging, troublesome, improper, offensive, lewd, pornographic, pedophilic, slanderous, intrusive of one's privacy, detestable, or culturally, nationally disagreeable,&nbsp;<span style="color: #e66b47;">weary, connecting or boosting money betting or laundering and</span>&nbsp;illegitimate activity of all nature; (c) hurting minors; (d) encroaching patents, trademarks, copyrights and other exclusive rights (e) violation of any law for the time being in force; (f) misinforming recipients and communicating information that is totally attacking or foreboding in nature; (g) personate another individual; (h) comprising software virus or any other computer code, files or programs premeditated to disturb, abolish or bound the function of any computer resource; (i) intimidating the harmony, honor, shield, safety or sovereignty of India, welcoming associations with states overseas, provocation to any crime, preventing investigation of any offence or is belittling any other country.</li>
+
+       <li>Credit/Debit Card Particulars You understand, approve, and confirm the accuracy of credit/debit card details provided by you for using our services on our website. Credit cards should be your own and should include correct and valid details. The info given by you will neither be used nor shared by us with any third party unless necessary for scam proofs by the law’s guideline or order of the court. We cannot be held responsible for any credit card fraud. You will be responsible for potential credit card fraud. The duty to prove wrongdoing shall be solely on you.</li>
+
+       <li>Trademarks&nbsp;<span style="color: #e66b47;">Trademarks, logos, and service marks are shown on the website are our possessions.</span> You are forbidden to use any mark for any purpose without the prior written permission from us or the third party. All of the information and content together with any software program on our website is secured by copyright. Users cannot transform, duplicate, dispense, spread, show, circulate, vend, certify, or generate unoriginal work by using any existing content on our website for marketable or community purposes.</li>
+
+       <li>Arbitration In the situation of any legal issue between the company and the customer over the course of individual use of the website in association with rationality, interpretation, execution, or purported breach of any provision of this agreement including the privacy policies and documents incorporated through reference, the dispute shall be mentioned to a sole arbitrator who will be an independent and neutral third party acknowledged by the Company.</li>
+       <li>Governing Law The agreement and privacy policy of documents incorporated by reference will be administrated and interpreted in accordance with the laws of India by exclusive jurisdiction conferred in the courts in Jodhpur.</li>
+
+       </ul>
+       </div>
---
+++ application/views/frontend/faq.php
@@ -0,0 +1,67 @@
+<div class="container">
+    <h2>About Royalhomes</h2>
+    <strong>Is Royalhomes An Indian Company?</strong>
+    <p>Yes. Royalhomes is a domestic brand for Basant. Basant is a well known exporter from Jodhpur exporting to 17+ countries worldwide for last 22 years.</p>
+    <strong>Where Is Royalhomes Head Office Located?</strong>
+    <p>The Head Office is in C-03/17,India Expo Mart, Greater Noida- 201306.</p>
+    <p>Mail id- <a href="mailto:care@Royalhomes">care@Royalhomes</a> and <a href="tel:Ph- +91 9001190790">Ph- +91 9001190790</a></p>
+    <strong>Are Royalhomes Products Available Internationally?</strong>
+    <p>Currently, we are not shipping single orders worldwide. But incase of bulk query we can export.</p>
+    <strong>About Our Products</strong> <strong>Where Are Your Manufacturing Facilities Located?</strong>
+    <p>Our manufacturing facility is located in Bhandu, Jodhpur.</p>
+    <strong>Are All Royalhomes Products Handmade?</strong>
+    <p>All the products are handmade and handcrafted inspired by Craft.</p>
+    <strong>Do Royalhomes Conform To International Norms?</strong>
+    <p>Yes all Lighting and Furniture conforms to International norms. In Lighting all fittings are CE certified, and the finishing is lead free. All the wood used in furniture is procured from legal source and passes Vriksh certification. All the products are tested as per international norms before getting packing.</p>
+    <strong>Shopping</strong> <strong>How Will I Know If You Have Received My Order?</strong>
+    <p>During the payment process you will be given a confirmation that your payment has been successfully processed. You will also be given an order confirmation number through an email from Royalhomes.</p>
+    <strong>How Do I Check My Order Status?</strong>
+    <p>You can view your order status at any time once you have placed your order, through My Orders options after logging in to your Royalhomes account on Royalhomes website. We try and dispatch all the orders from our warehouse within three working days and then you can track your order from Track your order option on the homepage of www.Royalhomes.in through the Fedex tracking system.</p>
+    <strong>Is It Safe To Use My Credit Card Online At Royalhomes?</strong>
+    <p>We do not store any card information on our website. Your card information is safe with our payment solution providers – Citrus and PayU.</p>
+    <strong>Shipping</strong> <strong>Will I Be Charged For Octroi Or Entry Taxes In Case Of Domestic Shipping?</strong>
+    <p>All of Royalhomes orders are shipped within India from our warehouse in Jodhpur, India to your provided shipping address. All freight is in built in the costing. The majority of orders will not have to pay any additional fees. In case these is any extra charge on some pincodes it will be shown in the invoice separately at the time of payment on the website.</p>
+    <strong>How Long Will It Take To Receive My Order?</strong>
+    <p>We make every effort to process your order as quickly as possible. We usually require 2-3 working days to dispatch your order. The order should ideally reach you within 10 days. But in some cases it might take 12-15 working days, in such cases we proactively call/ email the customer to inform the status of their product in transit. You can also mail us at <a href="mailto:care@Royalhomes">care@Royalhomes</a> or call us at <a href="tel:+91
+          9001190790">ph- +91 9001190790</a> for more details on the order.</p>
+    <strong>Do You Offer Expedited Shipping?</strong>
+    <p>No. At this point we are unable to offer overnight or expedited delivery service.</p>
+    <strong>How Is My Order Shipped?</strong>
+    <p>All Royalhomes orders are shipped door to door, via Fedex, India to your provided shipping address.</p>
+    <strong>Will Everything Ship Together?</strong>
+    <p>Yes, whatever is in stock will be shipped together.</p>
+    <strong>Can I Ship Different Items In My Order To Different Shipping Addresses?</strong>
+    <p>We can only process one shipping address per order. So, if you would like to order several items and ship to different people, please treat these as separate orders.</p>
+    <strong>Can My Order Ship To A Post Office Box?</strong>
+    <p>No. Since we offer door to door deliveries through our shipping partners therefore you must include full address details including a street address for the shipping address.</p>
+    <strong>Do you provide free shipping?</strong>
+    <p>Yes we provide free shipping.</p>
+    <strong>How Do I Track My Order?</strong>
+    <p>You can track your order on our Order Tracking page. If you do not have a tracking number or delivery confirmation number, please contact us. If you are not sure whether or not your order has shipped yet, you can check the status of your recent orders in My Account</p>
+    <strong>Billing &amp; Payments</strong> <strong>What Payment Methods Do You Accept?</strong>
+    <p>We accept all MasterCard, VISA, American Express, and Citibank Maestro Debit Cards (an online real-time authorization is done through the Payment Gateway). If you have an Indian bank account, you can also transfer your payment directly to us using our payment gateway's net banking facility.</p>
+    <strong>Customer Service</strong> <strong>How Do I Contact Customer Service?</strong>
+    <p>Please email or call us through our Contact Us page.</p>
+    <strong>How Do I Remove My Name From Your Email List?</strong>
+    <p>Please go to our unsubscribe page.</p>
+    <strong>Will You Share My Information With Others?</strong>
+    <p>No, we will not share your information with others. For more details, please read our Privacy Policy.</p>
+    <strong>If I Have Problems With My Order, Whom Should I Contact?</strong>
+    <p>If you have any problems or concerns about your order, please email us at&nbsp;<a href="mailto:care@Royalhomes">care@Royalhomes or contact us at </a><a href="+91 9001190790">Ph- +91 9001190790</a></p>
+    <strong>How Can I Modify My Order?</strong>
+    <p>You can modify your order at any time during the shopping process by editing / modifying your shopping cart. Once you confirm payment for the items in your shopping cart, the order is considered as final and cannot be modified.</p>
+    <strong>How Can I Cancel My Order?</strong>
+    <p>Once you confirm payment of your order, your order cannot be changed or cancelled. You will need to contact us on <a href="mailto:care@Royalhomes">care@Royalhomes</a> within 12 hours of placing your order for us to be able to confirm this cancellation. In case we have already processed your order you will be advised that the cancellation is not possible. As we aim to maintain quick turnaround times on our order processing, dispatch and deliveries, we may not be able to support your request unless it is received within the stipulated time frame. Cancellations are solely at the discretion of Royalhomes.</p>
+    <strong>Returns &amp; Exchanges</strong> <strong>What Is Your Return Policy?</strong>
+    <p>1. Returns of goods will be accepted only for Damaged/Defective products.&nbsp; 2. Replacement shall be considered only if the damage/defect is reported to us with photographic evidence in writing within 3 working days from the receipt of product.&nbsp; 3. Refund will be provided in case replacement for the same product is not available.&nbsp; 4. We normally process the refund in 5-6 days from the date of receipt of material at our end.</p>
+    <strong>When Will I Receive A Refund For My Returned Products?</strong>
+    <p>All credits will be issued only to the credit/debit card used for making the original purchase. Once we receive your returned item(s) we will issue your credit and send you an email confirmation with 3-4 working days. Please allow one billing cycle for the credit to appear on your credit/debit card statement.</p>
+    <strong>How Do I Exchange An Item?</strong>
+    <p>We do not have an option of exchanging an item. Product once received in good quality cannot be returned for exchange.</p>
+    <strong>Gift Services</strong> <strong>Can I Have The Product Gift Wrapped?</strong>
+    <p>In the future we hope to offer this service.</p>
+    <strong>Can I Include A Gift Message With The Package?</strong>
+    <p>Yes, a gift message can be included with your order.- please write to <a href="mailto:care@Royalhomes">care@Royalhomes</a> or call us at <a href="tel:+91 9001190790">Ph- +91 9001190790</a></p>
+    <strong>Do You Have A Gift Registry?</strong>
+    <p>We do not currently offer a gift registry but plan to add this feature in the near future.&nbsp;</p>
+    </div>
